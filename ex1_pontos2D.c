30f0c97237d8fcda63bcb8ef50c579b03b20847c
Marcelo Augusto Silva Belisário <marcelobeli9@gmail.com>
2023-05-03 22:29:42 -0300

    atualizacao de comentarios

--- ex1_pontos2D.c
+++ ex1_pontos2D.c
@@ -5,25 +5,23 @@
 #include <time.h>
 
 int main(){
+    // Usado com base na hora do relógio do Windows para criar valores aleatórios para algo.
     srand(time(NULL));
 
+    // Declaração dos pontos e variaveis úteis
     ponto2D* pontos[10];
     ponto2D *p, *p_min = NULL;
     float dist, dist_min = INFINITY;
 
+    // Cria valores aleatorios entre -15 e 15 para um ponto.
     for (int i=0; i<10; i++){
         pontos[i] = criar_p (rand()%31 -15, rand()%31 -15);
     }
 
-    for(int i = 0; i < 10; i++) {
-        p = pontos[i];
-        dist = calcular_distancia(p, criar_p(0, 0));
-        if(dist < dist_min) {
-            dist_min = dist;
-            p_min = p;
-        }
-    }
 
+    // Estrutura de repetição que exibe o ponto original,
+    // um novo ponto simétrico ao eixo X, um novo ponto simétrico ao eixo Y
+    // e exibe também o quadrante referente ao ponto principal.
     for (int i=0; i<10; i++){
         ponto2D *sx = simX_p(pontos[i]);
         ponto2D *sy = simY_p(pontos[i]);
@@ -33,10 +31,22 @@ int main(){
         printf("\nquadrante: %d\n\n", quadrante_p(pontos[i]));
     }
 
+    // Estrutura de repetição que verifica qual o ponto mais próximo da origem.
+    for(int i = 0; i < 10; i++) {
+        p = pontos[i];
+        dist = calcular_distancia(p, criar_p(0, 0));
+        if(dist < dist_min) {
+            dist_min = dist;
+            p_min = p;
+        }
+    }
+
+    //  Exibe o ponto mais próximo da origem.
     printf("Ponto mais proximo da origem: ");
     exibir_p(p_min);
     printf("\n");
 
+    // Faz a limpeza da memória alocada para os pontos criados.
     for(int i = 0; i < 10; i++) {
         free(pontos[i]);
     }
